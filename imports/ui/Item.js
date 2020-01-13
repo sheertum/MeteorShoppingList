340c4947a7c24a3afc46fd9c5eb363cdbdc515a3
sheertum <simonvanheertum@gmail.com>
2020-01-13 15:51:46 +0100

    Added checking on/off of items

--- imports/ui/Item.js
+++ imports/ui/Item.js
@@ -5,16 +5,29 @@ import { Items } from '../api/items.js';
 
 export default class Item extends Component {
     render() {
+        const itemClassName = this.props.item.checked ? 'checked' : '';
         return (
-        <li> 
+        <li className={itemClassName}> 
             <button className='delete' onClick={ this.deleteItem.bind(this)}>
             &times;
             </button>
-            {this.props.item.text}
+            <input
+                type="checkbox"
+                readOnly
+                checked={!!this.props.item.checked}
+                onClick={this.toggleChecked.bind(this)}
+            />
+            <span className="text">{this.props.item.text}</span>
             <br />  
             <b>{this.props.item.username}</b>
         </li>
         );
+    }
+
+    toggleChecked() {
+        Items.update(this.props.item._id, {
+            $set: { checked: !this.props.item.checked },
+        });
     }
 
     deleteItem() {
